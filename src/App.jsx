1542912cb0fa8b6306422b3bdcbd0b48370bc987
conancos <15524757+conancos@users.noreply.github.com>
2024-12-19 14:48:59 +0100

    styling clock and Implement Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Clock from './components/Clock'
+import Footer from './components/Footer'
 //import './components/styles/buttons-break.css'
 //import './components/styles/buttons-session.css'
 //import './components/styles/clock.css'
@@ -12,7 +13,7 @@ function App() {
     <>
       {/* <h1 style={{color: '#515a', textShadow: '2px 2px 4px #000'}}><u>CRONO POMODORO</u></h1> */}
       <Clock />
-      
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './styles/footer.css'
+
+const Footer = () => {
+
+    return (
+        <footer className="footer">
+            <p>Copyright © 2024 <a href="https://www.linkedin.com/in/joaquin-martinez-cortes">CONANCOS.dev</a> - bajo licencia <a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License v3.0</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
